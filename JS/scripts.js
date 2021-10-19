f650ae8e542e9f848c58c0fea76dfd05c2138575
Eugggld <eugenialopezd@gmail.com>
2021-10-19 13:46:28 +0200

    Added JS/scripts.js. And CSS/.DS_Store.

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello world');
+
+let favoriteFood = 'Canelons';
+document.write(favoriteFood);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
