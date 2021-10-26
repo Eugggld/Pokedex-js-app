ad35e220a0943e48048d9a25711797e9c7153304
Eugggld <eugenialopezd@gmail.com>
2021-10-26 13:25:46 +0200

    Added "for Loop" on script.js. Added "document.write" in Loop to make List-names and Height visible on Output.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList = [
-  {name: 'Charmander', height:0.6, type: 'fire'},
-  {name: 'Charmeleon', height:3.07, type: 'fire'},
+  {name: 'Charmander', height:0.6, type: ['fire']},
+  {name: 'Charmeleon', height:3.07, type: ['fire']},
   {name: 'Charizard', height:5.07, type: ['fire','flying']},
 ];
+
+//Listing the name and height of each pokemon in the array. Making Names and one Height visible on Output.
+//Adding for Loop and if Conditional.
+for (let i=0;i<pokemonList.length;i++){
+	document.write("<br><br>" + pokemonList[i].name + "<br>");
+  if (pokemonList[i].height > 5.0) document.write("  - Wow, thats tall!" + "<br>");
+  else document.write("<br>");
+}
