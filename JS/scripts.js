e2227a76d42a05ba6aa05e7846b718ce5859aea6
Eugggld <eugenialopezd@gmail.com>
2021-10-27 11:44:45 +0200

    Added Height and Changes after  Correction of Task.

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,8 +6,8 @@ let pokemonList = [
 
 //Listing the name and height of each pokemon in the array. Making Names and one Height visible on Output.
 //Adding for Loop and if Conditional.
-for (let i=0;i<pokemonList.length;i++){
-	document.write("<br><br>" + pokemonList[i].name + "<br>");
-  if (pokemonList[i].height > 5.0) document.write("  - Wow, thats tall!" + "<br>");
-  else document.write("<br>");
+//Adapting changes after Correction Task.
+for (let i = 0;i < pokemonList.length;i++){
+  if (pokemonList[i].height > 5.0) document.write("<br><br>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, thats tall!" + "<br>");
+  else document.write("<br><br>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
 }
