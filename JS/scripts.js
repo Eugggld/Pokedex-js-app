0054ceedd17c028dbe0e5d5802c4360b4cd66b2f
Eugggld <eugenialopezd@gmail.com>
2021-11-02 16:18:15 +0100

    Rearranged Pokemon's Array. Added more typeof. Added function addListItem and variables inside of it. Added addListitem in Return. Created new function showDetails.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,32 +1,69 @@
 let pokemonRepository = (function () {
   let pokemonList = [
-    {name: 'Charmander', height:0.6, type: ['fire']},
-    {name: 'Charmeleon', height:3.07, type: ['fire']},
-    {name: 'Charizard', height:5.07, type: ['fire','flying']},
-  ];
+    { name: 'Charmander',
+    height:0.6,
+    type: ['fire']
+  },
+  { name: 'Charmeleon',
+  height:3.07,
+  type: ['fire']
+},
+{
+  name: 'Charizard',
+  height:5.07,
+  type: ['fire','flying']
+},
+];
 
-  function getAll() {
-    return pokemonList;
+function add(pokemon) {
+  if (
+    typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "height" in pokemon &&
+    "type" in pokemon
+  ) {
+    pokemonList.push(pokemon);
+  } else {
+    console.log("Pokemon NOT correct!");
+  }
+}
+function getAll() {
+  return pokemonList;
+}
+
+function addListItem(pokemon){
+  let pokemonList = document.querySelector ('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
+  listItem.appendChild(button);
+  pokemonList.appendChild(listItem);
+  //button.addEventListener("click", function(event) {
+    //showDetails(pokemon); //add event listener. Listen to click. Call showDetails as handler function. Thus you pass the pokemon object as a paramenter when a pokemon is clicked. Should be same parameter as in addListItem().
   }
 
-  function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon
-    ) {
-      pokemonList.push(pokemon);
-    } else {
-      console.log("Pokemon NOT correct");
-    }
+  function showDetails(pokemon){
+    console.log(item); //Inside the function, run a console.log() on the Pokémon object that’s passed as the parameter. You’ll do more than just log the data in a later task.
   }
 
   return {
     getAll: getAll,
     add: add,
+    addListItem: addListItem,
   };
 })();
 
+pokemonRepository.add({ name: 'Pidgeon', height: 1, type: ['flying'] });
+pokemonRepository.add({ name: 'Squirtle', height: 1, type: ['water'] });
+pokemonRepository.add({ height: 1, type: ['water'] }); //Pokemon NOT correct
+
 console.log( pokemonRepository.getAll() );
+
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
 //Listing the name and height of each pokemon in the array. Making Names and one Height visible on Output.
 //Adding for Loop and if Conditional.
 //Adapting changes after Correction Task.
@@ -44,16 +81,12 @@ console.log( pokemonRepository.getAll() );
   console.log(pokemonList[property]);
 }); */
 
-pokemonRepository.add({ name: 'Pigeon', height: 1, type: ['flying'] });
 
-pokemonRepository.add({ name: 'Squirtle', height: 1, type: ['water'] });
-
-pokemonRepository.add({ height: 1, type: ['water'] }); //Pokemon NOT correct
-
+/*
 pokemonRepository.getAll().forEach(function (pokemon) {
   if (pokemon.height > 5.0) document.write("<br><br>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, thats tall!" + "<br>");
   else document.write("<br><br>" + pokemon.name + " (height: " + pokemon.height + ")" + "<br>");
-});
+});*/
 
 // NO need to add new function. See line 40 and 42:pokemonRepository.add({})
  /* function add(name, type) {
