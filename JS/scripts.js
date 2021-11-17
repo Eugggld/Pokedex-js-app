7ef9fc031e90f4278fc86c50689cdf83fc088f9e
Eugggld <eugenialopezd@gmail.com>
2021-11-17 18:32:08 +0100

    Added changes. Still not working.

--- JS/scripts.js
+++ JS/scripts.js
@@ -138,7 +138,7 @@ let pokemonRepository = (function() {
     let listPokemon = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add("button-class");
+    button.classList.add("button-class"); // instead of the older ("button-class")
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
     button.addEventListener("click", function (event) {
